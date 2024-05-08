f2ea7414a4109e88f477af529602742af9d8fdab
OnyxAnanke <onyxanankevt@gmail.com>
2024-05-08 14:47:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -110,7 +110,7 @@ export default {
         },
         {
           "callsign": "Kal",
-          "alias": "Vier Flau",
+          "alias": "TheDevilDen",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
@@ -118,7 +118,7 @@ export default {
         },
         {
           "callsign": "Starboard",
-          "alias": "Maria",
+          "alias": "Baranzar Cobral",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
@@ -126,7 +126,7 @@ export default {
         },
         {
           "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "alias": "JDMOSS_",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
           "frame": "Everest",
@@ -134,7 +134,7 @@ export default {
         },
         {
           "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
+          "alias": 'Mute Giggling',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
